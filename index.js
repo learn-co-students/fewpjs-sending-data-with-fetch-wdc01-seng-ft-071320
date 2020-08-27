fc56a2b63e0c2e77c4b12324242b7566d70efc81
slee713 <steven.lee1393@gmail.com>
2020-08-27 14:26:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+    let url = "http://localhost:3000/users"
+    function submitData(name, email) {
+        let config = {
+            method: "POST",
+            headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    };
+    
+
+    
+    
+    
+    return fetch(url, config)
+    .then(resp => resp.json())
+    .then(user =>  {
+        document.body.innerHTML =` <div> ${user.id} </div>`
+    })
+    .catch(function(error) {
+       document.body.innerHTML = `<div> ${error.message} </div>`;
+    })
+    
+    
+    }
+
